0f38cb1b7dc76dee46bd3d4fd89a9ed9316b5e51
Mickael Metesreau <mickael.metesreau@gmail.com>
2020-10-12 15:14:40 +0200

    init

---
+++ 01Layouts/MainActivity.cs
@@ -0,0 +1,105 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Widget;
+using System;
+using System.Linq;
+
+namespace _01Layouts
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btn4;
+        private Button btn5;
+        private Button btn6;
+        private Button btn7;
+        private Button btn8;
+        private Button btn9;
+        private Button btn0;
+        private Button btnPlus;
+        private Button btnEqual;
+        private TextView currentTextview;
+        private TextView resultTextview;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_main);
+
+            btn1 = FindViewById<Button>(Resource.Id.button1);
+            btn2 = FindViewById<Button>(Resource.Id.button2);
+            btn3 = FindViewById<Button>(Resource.Id.button3);
+            btn4 = FindViewById<Button>(Resource.Id.button4);
+            btn5 = FindViewById<Button>(Resource.Id.button5);
+            btn6 = FindViewById<Button>(Resource.Id.button6);
+            btn7 = FindViewById<Button>(Resource.Id.button7);
+            btn8 = FindViewById<Button>(Resource.Id.button8);
+            btn9 = FindViewById<Button>(Resource.Id.button9);
+            btn0 = FindViewById<Button>(Resource.Id.button0);
+            btnPlus = FindViewById<Button>(Resource.Id.buttonPlus);
+            btnEqual = FindViewById<Button>(Resource.Id.buttonEqual);
+
+            currentTextview = FindViewById<TextView>(Resource.Id.textview_current);
+            resultTextview = FindViewById<TextView>(Resource.Id.textview_result);
+
+            currentTextview.Text = string.Empty;
+            resultTextview.Text = string.Empty;
+
+            btn1.Click += Btn_Click;
+            btn2.Click += Btn_Click;
+            btn3.Click += Btn_Click;
+            btn4.Click += Btn_Click;
+            btn5.Click += Btn_Click;
+            btn6.Click += Btn_Click;
+            btn7.Click += Btn_Click;
+            btn8.Click += Btn_Click;
+            btn9.Click += Btn_Click;
+            btn0.Click += Btn_Click;
+            btnPlus.Click += Btn_Click;
+
+            btnEqual.Click += BtnEqual_Click;
+        }
+
+        private void BtnEqual_Click(object sender, EventArgs e)
+        {
+            var operations = currentTextview
+                                              .Text
+                                              ?.Split('+')
+                                              .Where(x => !string.IsNullOrEmpty(x))
+                                              .ToList();
+
+            if (!operations.Any())
+                Toast.MakeText(this, "La saisie est vide", ToastLength.Short).Show();
+
+            else
+            {
+                resultTextview.Text = operations
+                                              .Select(int.Parse)
+                                              .Sum()
+                                              .ToString();
+
+                currentTextview.Text = string.Empty;
+            }
+        }
+
+        private void Btn_Click(object sender, System.EventArgs e)
+        {
+            currentTextview.Text += ((Button)sender).Text;
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ 02Lists/MainActivity.cs
@@ -0,0 +1,88 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Collections.Generic;
+using Java.Lang;
+using Android.Views;
+using Android.Support.V7.Widget;
+
+namespace _02Lists
+{
+
+    public class CustomAdapter : BaseAdapter
+    {
+        private readonly Activity context;
+        private readonly List<string> lst;
+
+        public CustomAdapter(Activity context, List<string> lst)
+        {
+            this.context = context;
+            this.lst = lst;
+        }
+
+        public override int Count => lst.Count;
+
+        public override Object GetItem(int position)
+        {
+            return lst[position];
+        }
+
+        public override long GetItemId(int position)
+        {
+            return -1;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var view = convertView ?? context.LayoutInflater.Inflate(Resource.Layout.listitem_simplelistitem, parent, false);
+
+            var textview = view.FindViewById<TextView>(Resource.Id.listitem_text1);
+
+            textview.Text = lst[position];
+
+            return view;
+        }
+    }
+
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private List<string> data;
+        private BaseAdapter adapter;
+        private ListView listview;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_main);
+
+            listview = FindViewById<ListView>(Resource.Id.listview);
+
+            data =  new List<string> { "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3" };
+            
+            adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, data);
+            
+            listview.Adapter = adapter;
+
+            listview.ItemClick += Listview_ItemClick;
+        }
+
+        private void Listview_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            var item = adapter.GetItem(e.Position);
+
+            Toast.MakeText(this, $"Element selectionnée {item}", ToastLength.Short).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
